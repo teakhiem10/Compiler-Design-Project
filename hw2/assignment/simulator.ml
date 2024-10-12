3926b3414d84e807f3fd3167127be1024d199874
tburckhardt <burckhardttim@gmail.com>
2024-10-12 14:50:04 +0200

    Formatted with ocp-indent

--- hw2/assignment/simulator.ml
+++ hw2/assignment/simulator.ml
@@ -95,7 +95,7 @@ let sbytes_of_int64 (i:int64) : sbyte list =
   let open Char in 
   let open Int64 in
   List.map (fun n -> Byte (shift_right i n |> logand 0xffL |> to_int |> chr))
-           [0; 8; 16; 24; 32; 40; 48; 56]
+    [0; 8; 16; 24; 32; 40; 48; 56]
 
 (* Convert an sbyte representation to an int64 *)
 let int64_of_sbytes (bs:sbyte list) : int64 =
@@ -143,28 +143,30 @@ let sbytes_of_data : data -> sbyte list = function
 let debug_simulator = ref false
 
 (* Interpret a condition code with respect to the given flags. *)
-let interp_cnd {fo; fs; fz} : cnd -> bool = fun (c:cnd) -> begin match c with
-                                                            | Eq -> fz
-                                                            | Neq -> not fz
-                                                            | Gt -> (not fz) && fs = fo
-                                                            | Ge -> fs = fo
-                                                            | Lt -> not(fs = fo)
-                                                            | Le -> not(fs = fo) || fz
-                                                            end
+let interp_cnd {fo; fs; fz} : cnd -> bool = fun (c:cnd) -> 
+  begin match c with
+    | Eq -> fz
+    | Neq -> not fz
+    | Gt -> (not fz) && fs = fo
+    | Ge -> fs = fo
+    | Lt -> not(fs = fo)
+    | Le -> not(fs = fo) || fz
+  end
 
 (* Maps an X86lite address into Some OCaml array index,
    or None if the address is not within the legal address space. *)
-let map_addr (addr:quad) : int option = if addr < mem_bot || addr >= mem_top then   
-                                          None  
-                                        else  
-                                          Some (Int64.to_int(Int64.sub addr mem_bot))
+let map_addr (addr:quad) : int option = 
+  if addr < mem_bot || addr >= mem_top then   
+    None  
+  else  
+    Some (Int64.to_int(Int64.sub addr mem_bot))
 
 (* Simulates one step of the machine:
-    - fetch the instruction at %rip
-    - compute the source and/or destination information from the operands
-    - simulate the instruction semantics
-    - update the registers and/or memory appropriately
-    - set the condition flags
+   - fetch the instruction at %rip
+   - compute the source and/or destination information from the operands
+   - simulate the instruction semantics
+   - update the registers and/or memory appropriately
+   - set the condition flags
 *)
 
 let get_register_value (m:mach) (reg:reg) : int64 = 
@@ -200,14 +202,14 @@ let get_num_from_operand (m:mach) (op:operand): int64 =
   match op with
   | Imm imm -> 
     (match imm with
-    | Lit lit -> lit
-    | _ -> failwith "Cannot get number from label"
+     | Lit lit -> lit
+     | _ -> failwith "Cannot get number from label"
     )
   | Reg reg -> get_register_value m reg
   | Ind1 imm -> 
     (match imm with
-    | Lit lit -> get_memory_value m lit
-    | _ -> failwith "Cannot get value of label"
+     | Lit lit -> get_memory_value m lit
+     | _ -> failwith "Cannot get value of label"
     )
   | _ -> failwith "false instruction detected:"
 
@@ -216,15 +218,15 @@ let store_value_at_operand (m:mach) (op:operand) (data:int64) : unit =
   | Reg reg -> store_in_register m reg data
   | Ind1 imm -> 
     (match imm with
-    | Lit lit -> store_in_memory m lit data
-    | _ -> failwith "Cannot store at label"
+     | Lit lit -> store_in_memory m lit data
+     | _ -> failwith "Cannot store at label"
     )
   | _ -> failwith "Cannot store at immediate"
 
 let increment_rip (m:mach) : unit = 
-    store_in_register m Rip (Int64.add 8L (get_register_value m Rip))
+  store_in_register m Rip (Int64.add 8L (get_register_value m Rip))
 
-    (* Make all indirect operands into Ind1 *)
+(* Make all indirect operands into Ind1 *)
 let handle_operands (m:mach) (operands: operand list) : operand list = 
   let handle_operand (op:operand) : operand = 
     match op with 
@@ -234,8 +236,8 @@ let handle_operands (m:mach) (operands: operand list) : operand list =
     | Ind2 register -> Ind1 (Lit (get_register_value m register))
     | Ind3 (imm, register) -> 
       (match imm with
-      | Lit lit -> Ind1 (Lit (Int64.add (get_register_value m register)  lit))
-      | _ -> failwith "Cannot handle label in handle_operands"
+       | Lit lit -> Ind1 (Lit (Int64.add (get_register_value m register)  lit))
+       | _ -> failwith "Cannot handle label in handle_operands"
       )
   in
   List.map handle_operand operands
@@ -247,48 +249,48 @@ let get_sign (num: int64) : int = get_bit num 63
 
 let truncate_and_get_flags (num: Big_int.big_int): (bool * bool * bool * int64) = 
   let oF = Big_int.gt_big_int num (Big_int.big_int_of_int64 Int64.max_int)
-        || Big_int.lt_big_int num (Big_int.big_int_of_int64 Int64.min_int) in
+           || Big_int.lt_big_int num (Big_int.big_int_of_int64 Int64.min_int) in
   let modulus = Big_int.shift_left_big_int Big_int.unit_big_int 64 in
   let wrapped = Big_int.mod_big_int num modulus in
   let result = 
-  if Big_int.le_big_int wrapped (Big_int.big_int_of_int64 Int64.max_int) then
-    Big_int.int64_of_big_int wrapped
-  else
-    Big_int.int64_of_big_int (Big_int.sub_big_int wrapped modulus)
+    if Big_int.le_big_int wrapped (Big_int.big_int_of_int64 Int64.max_int) then
+      Big_int.int64_of_big_int wrapped
+    else
+      Big_int.int64_of_big_int (Big_int.sub_big_int wrapped modulus)
   in
   let sF = 1 = (get_sign result) in
   let zF = Int64.equal 0L result in 
   (oF, sF, zF, result)
 
 let handle_unary_exp (op:opcode) (num:int64) : Big_int.big_int = 
-    let bNum = Big_int.big_int_of_int64 num in
-    match op with
-    | Negq -> Big_int.sub_big_int Big_int.zero_big_int bNum
-    | Incq -> Big_int.add_big_int bNum Big_int.unit_big_int
-    | Decq -> Big_int.sub_big_int bNum Big_int.unit_big_int
-    | Notq -> Big_int.big_int_of_int64 (Int64.lognot num)
-    | _ -> failwith "binary instruction detected:"
-  
+  let bNum = Big_int.big_int_of_int64 num in
+  match op with
+  | Negq -> Big_int.sub_big_int Big_int.zero_big_int bNum
+  | Incq -> Big_int.add_big_int bNum Big_int.unit_big_int
+  | Decq -> Big_int.sub_big_int bNum Big_int.unit_big_int
+  | Notq -> Big_int.big_int_of_int64 (Int64.lognot num)
+  | _ -> failwith "binary instruction detected:"
+
 let handle_binary_exp (op:opcode) (num1:int64) (num2:int64) : Big_int.big_int = 
-    let b1 = Big_int.big_int_of_int64 num1 in
-    let b2 = Big_int.big_int_of_int64 num2 in
-    match op with
-    | Addq -> Big_int.add_big_int b2 b1
-    | Subq -> Big_int.sub_big_int b2 b1
-    | Imulq -> Big_int.mult_big_int b2 b1
-    | Andq -> Big_int.big_int_of_int64(Int64.logand num2 num1)
-    | Orq -> Big_int.or_big_int b2 b1
-    | Xorq -> Big_int.xor_big_int b2 b1
-    | Sarq | Shlq | Shrq -> 
-      let amt = Int64.to_int num1 in
-      let result = 
-        match op with
-        | Sarq -> Int64.shift_right num2 amt
-        | Shlq -> Int64.shift_left num2 amt
-        | Shrq -> Int64.shift_right_logical num2 amt
-        | _ -> failwith "non-shift instruction detected:"
-      in Big_int.big_int_of_int64 result
-    |_-> failwith "unary exp detected:"
+  let b1 = Big_int.big_int_of_int64 num1 in
+  let b2 = Big_int.big_int_of_int64 num2 in
+  match op with
+  | Addq -> Big_int.add_big_int b2 b1
+  | Subq -> Big_int.sub_big_int b2 b1
+  | Imulq -> Big_int.mult_big_int b2 b1
+  | Andq -> Big_int.big_int_of_int64(Int64.logand num2 num1)
+  | Orq -> Big_int.or_big_int b2 b1
+  | Xorq -> Big_int.xor_big_int b2 b1
+  | Sarq | Shlq | Shrq -> 
+    let amt = Int64.to_int num1 in
+    let result = 
+      match op with
+      | Sarq -> Int64.shift_right num2 amt
+      | Shlq -> Int64.shift_left num2 amt
+      | Shrq -> Int64.shift_right_logical num2 amt
+      | _ -> failwith "non-shift instruction detected:"
+    in Big_int.big_int_of_int64 result
+  |_-> failwith "unary exp detected:"
 
 let handle_exp (m:mach) (op:opcode) (operands: operand list) : (int * int * int * int64) = 
   let aux = get_num_from_operand m in
@@ -298,7 +300,7 @@ let handle_exp (m:mach) (op:opcode) (operands: operand list) : (int * int * int 
     | Negq | Incq | Decq | Notq -> handle_unary_exp op (List.hd numbers)
     | Addq | Subq | Imulq | Andq | Orq | Xorq | Sarq | Shlq | Shrq -> handle_binary_exp op (List.hd numbers) (List.nth numbers 1)
     | _ -> failwith "data movement instruction detected:"
-    in
+  in
   let (oF, sF, zF, result) = truncate_and_get_flags big_int_result in 
   let sf_return = 
     match op with
@@ -321,12 +323,12 @@ let handle_exp (m:mach) (op:opcode) (operands: operand list) : (int * int * int 
     | Notq -> -1
     | Addq | Imulq | Incq  -> Bool.to_int oF
     | Sarq | Shlq | Shrq -> if (Int64.equal (List.hd numbers) 0L) || not (Int64.equal (List.hd numbers) 1L) then -1 else 
-      begin match op with
-      | Sarq -> 0
-      | Shlq -> if (get_bit (List.nth numbers 1) 63) = (get_bit (List.nth numbers 1) 62) then 0 else 1
-      | Shrq -> get_sign (List.nth numbers 1)
-      | _ -> failwith "data movement instruction detected:"
-      end
+        begin match op with
+          | Sarq -> 0
+          | Shlq -> if (get_bit (List.nth numbers 1) 63) = (get_bit (List.nth numbers 1) 62) then 0 else 1
+          | Shrq -> get_sign (List.nth numbers 1)
+          | _ -> failwith "data movement instruction detected:"
+        end
     | _-> failwith "data movement instruction detected:"
   in 
   (of_return, sf_return, zF_return, result)
@@ -337,41 +339,41 @@ let handle_data_movement (m:mach) (op:opcode) (operands: operand list) : unit =
     match op with 
     | Leaq -> let ind = operand1 in 
       (match ind with
-      | Ind1 imm -> 
-        (match imm with 
-        | Lit lit -> lit
-        | _ -> failwith "Cannot interpret label"
-        )
-      | _ -> failwith "Operand should be simplified to Ind1 before passing to this function"
+       | Ind1 imm -> 
+         (match imm with 
+          | Lit lit -> lit
+          | _ -> failwith "Cannot interpret label"
+         )
+       | _ -> failwith "Operand should be simplified to Ind1 before passing to this function"
       )
     | Movq -> get_num_from_operand m operand1
     | Pushq -> get_num_from_operand m operand1
     | Popq -> get_memory_value m (get_register_value m Rsp)
     | _ -> failwith "non_data-movement instruction detected:"
   in 
-    match op with 
-    | Leaq | Movq -> 
-      let operand2 = List.nth operands 1 in 
-      (match operand2 with
-      | Reg reg -> store_in_register m reg result
-      | Ind1 imm -> 
-        (match imm with 
+  match op with 
+  | Leaq | Movq -> 
+    let operand2 = List.nth operands 1 in 
+    (match operand2 with
+     | Reg reg -> store_in_register m reg result
+     | Ind1 imm -> 
+       (match imm with 
         | Lit lit -> store_in_memory m lit result
         | _ -> failwith "Cannot store into label"
-        )
-      | _ -> failwith "Invalid storage type"
-      )
-    | Pushq -> 
-      store_in_register m Rsp (Int64.sub (get_register_value m Rsp) 8L);
-      store_in_memory m (get_register_value m Rsp) result
-    | Popq -> 
-      (match operand1 with
-      | Reg reg -> 
-        (store_in_register m reg result;
+       )
+     | _ -> failwith "Invalid storage type"
+    )
+  | Pushq -> 
+    store_in_register m Rsp (Int64.sub (get_register_value m Rsp) 8L);
+    store_in_memory m (get_register_value m Rsp) result
+  | Popq -> 
+    (match operand1 with
+     | Reg reg -> 
+       (store_in_register m reg result;
         store_in_register m Rsp (Int64.add 8L (get_register_value m Rsp)))
-      | _ -> failwith "I think this is illegal?"
-      )
-    | _ -> failwith "non-data-movement instruction detected:"
+     | _ -> failwith "I think this is illegal?"
+    )
+  | _ -> failwith "non-data-movement instruction detected:"
 
 let handle_conditional (m:mach) (op:opcode) (cc:cnd) (operand:operand) : unit = 
   let cc_true = interp_cnd m.flags cc in
@@ -391,18 +393,18 @@ let handle_control_flow (m:mach) (op:opcode) (operands: operand list) : unit =
   | Retq -> handle_data_movement m Popq [Reg Rip]
   | _ -> let operand1 = List.hd operands in
     begin match op with
-    | Jmp -> store_in_register m Rip (get_num_from_operand m operand1)
-    | Callq -> 
-      increment_rip m;
-      handle_data_movement m Pushq [Reg Rip];
-      store_in_register m Rip (get_num_from_operand m operand1);
-    | _ -> failwith "something is wrong"
+      | Jmp -> store_in_register m Rip (get_num_from_operand m operand1)
+      | Callq -> 
+        increment_rip m;
+        handle_data_movement m Pushq [Reg Rip];
+        store_in_register m Rip (get_num_from_operand m operand1);
+      | _ -> failwith "something is wrong"
     end
 
 let update_flags (m:mach) (oF:int) (sF:int) (zF:int) : unit = 
   (if -1 = sF then () else m.flags.fs <- sF = 1; 
-  if -1 = zF then () else m.flags.fz <- zF = 1;
-  if -1 = oF then () else m.flags.fo <- oF = 1;)
+   if -1 = zF then () else m.flags.fz <- zF = 1;
+   if -1 = oF then () else m.flags.fo <- oF = 1;)
 
 let get_destination_for_expression (operands: operand list) : operand =
   if List.length operands > 1 then List.nth operands 1 else List.hd operands
@@ -423,14 +425,14 @@ let do_instruction (m:mach) (op:opcode) (operands: operand list) : unit =
 
 let step (m:mach) : unit =
   let ins_byte = get_instruction_from_memory m in
-    match ins_byte with 
-    | InsFrag | Byte _ -> failwith "Instruction pointer does not point to an instruction"
-    | InsB0 (opcode, operands) -> 
-      let simpler_operands = handle_operands m operands in
-      do_instruction m opcode simpler_operands;
-      match opcode with
-      | Jmp | Callq | Retq | J _ -> ()
-      | _ -> increment_rip m
+  match ins_byte with 
+  | InsFrag | Byte _ -> failwith "Instruction pointer does not point to an instruction"
+  | InsB0 (opcode, operands) -> 
+    let simpler_operands = handle_operands m operands in
+    do_instruction m opcode simpler_operands;
+    match opcode with
+    | Jmp | Callq | Retq | J _ -> ()
+    | _ -> increment_rip m
 
 (* Runs the machine until the rip register reaches a designated
    memory address. Returns the contents of %rax when the 
@@ -438,7 +440,7 @@ let step (m:mach) : unit =
 let run (m:mach) : int64 = 
   while m.regs.(rind Rip) <> exit_addr do step m done;
   m.regs.(rind Rax)
-  
+
 (* assembling and linking --------------------------------------------------- *)
 
 (* A representation of the executable *)
@@ -456,34 +458,34 @@ exception Undefined_sym of lbl
 exception Redefined_sym of lbl
 
 (* Convert an X86 program into an object file:
-    - separate the text and data segments
-    - compute the size of each segment
+   - separate the text and data segments
+   - compute the size of each segment
       Note: the size of an Asciz string section is (1 + the string length)
             due to the null terminator
 
-    - resolve the labels to concrete addresses and 'patch' the instructions to 
+   - resolve the labels to concrete addresses and 'patch' the instructions to 
       replace Lbl values with the corresponding Imm values.
 
-    - the text segment starts at the lowest address
-    - the data segment starts after the text segment
+   - the text segment starts at the lowest address
+   - the data segment starts after the text segment
 
-  HINT: List.fold_left and List.fold_right are your friends.
-  *)
+   HINT: List.fold_left and List.fold_right are your friends.
+*)
 let assemble (p:prog) : exec =
-failwith "assemble unimplemented"
+  failwith "assemble unimplemented"
 
 (* Convert an object file into an executable machine state. 
-    - allocate the mem array
-    - set up the memory state by writing the symbolic bytes to the 
+   - allocate the mem array
+   - set up the memory state by writing the symbolic bytes to the 
       appropriate locations 
-    - create the inital register state
-      - initialize rip to the entry point address
-      - initializes rsp to the last word in memory 
-      - the other registers are initialized to 0
-    - the condition code flags start as 'false'
+   - create the inital register state
+   - initialize rip to the entry point address
+   - initializes rsp to the last word in memory 
+   - the other registers are initialized to 0
+   - the condition code flags start as 'false'
 
-  Hint: The Array.make, Array.blit, and Array.of_list library functions 
-  may be of use.
+   Hint: The Array.make, Array.blit, and Array.of_list library functions 
+   may be of use.
 *)
 let load {entry; text_pos; data_pos; text_seg; data_seg} : mach = 
-failwith "load unimplemented"
+  failwith "load unimplemented"
